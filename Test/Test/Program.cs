9f38c44bd0c85de46565f552fb43e643e6d25ca7
TobiasRoland <tobiasroland@gmail.com>
2013-11-07 11:55:00 +0000

    Ny ændring

--- Test/Test/Program.cs
+++ Test/Test/Program.cs
@@ -13,7 +13,7 @@ namespace Test
             Console.WriteLine("Hello, World!");
             Console.WriteLine("Push med Git plugin til VS 12");
             Console.WriteLine("Test-commit //Fuck it");
-            for (int i = 0; i < 21; i++)
+            for (int i = 0; i < 16; i++)
             {
                 Console.WriteLine("Na");
             }

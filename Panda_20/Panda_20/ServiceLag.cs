52294fac20a054697118c7d8638721ef2f6b67dd
TobiasRoland <tobiasroland@gmail.com>
2013-11-13 12:12:36 +0000

    Der er nu en snl-fil, og en fil til service-laget.

---
+++ Panda_20/Panda_20/ServiceLag.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Panda_20
+{
+    class Service
+    {
+        //Here be methods soon @author Roland
+    }
+}

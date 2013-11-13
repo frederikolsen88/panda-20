6d1076b53cb480d2cc6cfeb393117cab4268bc21
Frederik Olsen <frederikolsen.dfl@gmail.com>
2013-11-13 15:02:56 +0100

    Tilføjet BrowserHelper, arbejder på navigated event

---
+++ Panda_20/Panda_20/gui/BrowserHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Panda_20.gui
+{
+    class BrowserHelper
+    {
+
+        public static Uri CurrentUri { get; set; }
+
+        public static void InitBrowser(WebBrowser browser)
+        {
+            // TODO URL fra AppValues
+            browser.Navigate("http://www.google.com");
+        }
+
+        
+    }
+}

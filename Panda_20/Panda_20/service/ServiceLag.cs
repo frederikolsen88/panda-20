55a572bfa677baeab1b006df3bd26509d5af16e8
TobiasRoland <tobiasroland@gmail.com>
2013-11-14 21:56:10 +0000

    Lidt stuff og nogen spørgsmål

--- Panda_20/Panda_20/service/ServiceLag.cs
+++ Panda_20/Panda_20/service/ServiceLag.cs
@@ -14,17 +14,22 @@ namespace Panda_20
     /// </summary>
     public sealed class Service
     {
-        //-----------------------------------------------------------
-        //---------------<SINGLETON>-------------------- Author: TRR
-        //-----------------------------------------------------------
 
         private static readonly Service ServiceInstance = new Service();
 
         // TODO Redundans; vi skal bruge XML'en alligevel
-        public string[] TokenAndExpiresIn { get; set; }
+
+        private String _facebookToken;
+        public String FacebookToken {
+            get
+            {
+                return _facebookToken;
+            } 
+        }
+
+        public string[] TokenAndExpiresIn { get; set; } //TODO hvad er det her? Token expires in... hvad tæller den? Hvad gør den? Hvordan gør den det? Kommentér lige, når I laver noget... mærkeligt.
         private readonly Dictionary<string, JsonObject> _pages;
         private FacebookClient _client;
-
         public JsonObject SelectedPage { get; set; }
 
         private Service()
@@ -44,7 +49,6 @@ namespace Panda_20
         //-----------------------------------------------------------
         //--------------<READ XML VALUE>---------------- Author: TRR 
         //-----------------------------------------------------------
-
         public String GetXmlElement(String elementName)
         {
 
@@ -59,14 +63,43 @@ namespace Panda_20
             return element.Value;
         }
 
+
+        //-----------------------------------------------------------
+        //--------------<SET FACEBOOK TOKEN>--------------Author: TRR 
+        //-----------------------------------------------------------
+        // Givet accesstoken'en, sætter den og opdaterer FacebookClient (og returner den)
+        
+        public FacebookClient SetFacebookToken(String accessToken)
+        {
+            try
+            {
+                this._facebookToken = accessToken;
+                FacebookClient newClient = new FacebookClient(accessToken);
+                this._client = newClient;
+            }
+            catch (FacebookOAuthException)
+            {
+                //TODO Her skal det håndteres, hvis brugeren har valgt "nej" til at give os rettigheder,
+                // eller hvis token'en er expired.
+                //... basically, brugeren skal redirectes til facebook og logge ind igen. 
+                // Eller skal vi bare lade exception'en blive kastet, og så fange den højere oppe? Hm...
+
+            }
+
+            return this._client;
+
+        }
+
         //-----------------------------------------------------------
         //--------------<GET FB CLIENT>----------------- Author: FOL 
         //-----------------------------------------------------------
 
-        private FacebookClient GetClient(string token)
+        //TODO bør denne metode tage en parameter med? Burde den ikke bare hente tokenen fra den globale "_facebookToken"-variabel? Ellers skal vi til at håndtere om tokenen er lovlig her også... skal vi ikke? - TRR
+        private FacebookClient GetClient(string token) 
         {
             return _client ?? (_client = new FacebookClient(token));
         }
+
 
         //-----------------------------------------------------------
         //----------<FETCH USER'S PAGES>---------------- Author: FOL 
@@ -74,6 +107,8 @@ namespace Panda_20
 
         public Dictionary<string, JsonObject> GetPages()
         {
+            //TODO For overskuelighed, omskriv det her, så den ikke henter token'en fra TokenExpiresIn[0], men i stedet bare bruger _client, hvor clienten er gemt.
+
             JsonObject response = GetClient(TokenAndExpiresIn[0]).Get("me/accounts") as JsonObject;
 
             if (_pages.Count == 0)
@@ -88,5 +123,7 @@ namespace Panda_20
 
             return _pages;
         } 
+
+        
     }
 }

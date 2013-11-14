22deb9c5e75950bf35eea85f1b90105371f348a3
Chilitoke <Chilitoke@gmail.com>
2013-11-14 13:42:12 +0100

    Tilføjet Drawing og Forms til References, skal bruges til notifyIcon og andre 'Windows UI' ting

--- Panda_20/Panda_20/MainWindow.xaml.cs
+++ Panda_20/Panda_20/MainWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Windows.Forms;
 
 namespace Panda_20
 {
@@ -21,6 +22,7 @@ namespace Panda_20
     /// </summary>
     public partial class MainWindow : Window
     {
+        private NotifyIcon notifyIcon;
         public MainWindow()
         {
             // THOR IS HERE!

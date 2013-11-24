2fff708607a4c448d05374e8a12f26cd6879bd59
TobiasRoland <tobiasroland@gmail.com>
2013-11-24 18:07:36 +0000

    Close + save working

--- Panda_20/Panda_20/OptionsWindow.xaml.cs
+++ Panda_20/Panda_20/OptionsWindow.xaml.cs
@@ -28,10 +28,17 @@ namespace Panda_20
             InitializeComponent();
         }
 
-        private void btnSave_Click(object sender, RoutedEventArgs e)
+        private void ButtonSave_Click(object sender, RoutedEventArgs e)
         {
             Settings.Default.Save(); //  TODO This saves any changes made to the app-wide settings; if anything else is being done, that has to be handled as well.
+            CloseOptionsWinodw(null, null);
 
         }
+
+        private void CloseOptionsWinodw(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
     }
 }

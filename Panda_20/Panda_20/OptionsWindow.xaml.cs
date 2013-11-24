bd05dd266cd231cc5074609a749a3fd67de256a6
TobiasRoland <tobiasroland@gmail.com>
2013-11-24 15:50:35 +0000

    Dropdown databinding ved notifications

--- Panda_20/Panda_20/OptionsWindow.xaml.cs
+++ Panda_20/Panda_20/OptionsWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using MahApps.Metro.Controls;
+using Panda_20.Properties;
 
 namespace Panda_20
 {
@@ -20,9 +22,16 @@ namespace Panda_20
     /// </summary>
     public partial class OptionsWindow : MetroWindow
     {
+
         public OptionsWindow()
         {
             InitializeComponent();
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            Settings.Default.Save(); //  TODO This saves any changes made to the app-wide settings; if anything else is being done, that has to be handled as well.
+
         }
     }
 }

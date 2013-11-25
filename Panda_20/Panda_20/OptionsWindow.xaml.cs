9e0b6932471181e2a181f771aae847700b56c959
Frederik Olsen <frederikolsen.dfl@gmail.com>
2013-11-25 09:54:59 +0100

    OptionsWindow er hvidt og lukker ikke ned ved save

--- Panda_20/Panda_20/OptionsWindow.xaml.cs
+++ Panda_20/Panda_20/OptionsWindow.xaml.cs
@@ -49,7 +49,7 @@ namespace Panda_20
         {
             Settings.Default.Save();
             //  TODO This saves any changes made to the app-wide settings; if anything else is being done, that has to be handled as well.
-            CloseOptionsWinodw(null, null);
+            // CloseOptionsWinodw(null, null);
 
         }
 

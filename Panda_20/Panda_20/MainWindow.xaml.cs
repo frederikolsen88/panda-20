1e2e1813dba78aebc08e7ead8d8da9a5a75ec1a7
TobiasRoland <tobiasroland@gmail.com>
2013-11-26 16:42:35 +0000

    Application is now single-instance

--- Panda_20/Panda_20/MainWindow.xaml.cs
+++ Panda_20/Panda_20/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 using System.Windows;
 using System.Windows.Forms;
 using Panda_20.gui;
@@ -13,6 +14,8 @@ namespace Panda_20
     /// 
     /// Author: Frederik Olsen og Toke V. Albrechtsen
     /// </summary>
+  
+    
     
     public partial class MainWindow : Window
     {
@@ -32,22 +35,50 @@ namespace Panda_20
         private OptionsWindow opWindow;
         public static bool OptionsShowing = false;
 
+
+        
+        // Mutex for allowing checking for a single isntance.
+        // A named mutex allows for stack synchronization across threads and processes, whih is just what we need - think of it as a lock.
+        // The name of it is just a random generated GUID from createguid.com, it could be "BatmanRules" if we wanted it to, but this should be unique (hopefully).
+        static Mutex singleInstanceMutex = new Mutex(true, "PANDA : {F8830A8E-8081-48CD-A280-B3C9BF7E7F5F}"); 
+
         public MainWindow()
         {
-            // SetupRoutine to check if this is first time and other fun stuff. FIRST THING THAT SHOULD HAPPEN IS THIS
-            SetupRoutine();
-            
-            
-            InitializeComponent();
+            // First parameter - Timespan.Zero = mutex waits 0 milliseconds to see if it can gain access to the mutex (so immideately returns false if mutex is locked).
+            // Second parameter - exitContext set to true, meaning we can escape the synchronization context before we try to aquire a lock on it (in case we don't get it right away, ie. when it's in use) 
+            if (singleInstanceMutex.WaitOne(TimeSpan.Zero, true))
+            {
+                //This wraps the application startup
+                InitiateMainWindow();
 
+                //when app is shut down, the mutex should be released.
+                singleInstanceMutex.ReleaseMutex();
+            }
+            else
+            {
+                // we could make the app "jump up" from minimized state here and show a main window, if we actually had one... which we don't so that's pointless.
+                // We could simply choose not to display anything. For now, a messagebox will do.
+                MessageBox.Show("This program is already running.");
+            }
+        }
+
+        /// <summary>
+        /// What would usually be contained in the Main-class, extracted for easier editing (so as not to have to consider Mutex when programming).
+        /// </summary>
+        private void InitiateMainWindow()
+        {
+            // TimesRunStartupRoutine to check if this is first time run and other fun stuff.
+            TimesRunStartupRoutine();
+            InitializeComponent();
             WindowState = System.Windows.WindowState.Minimized;
         }
+        
 
         /// <summary>
         ///  Invokes the method to handle first time run, if it is in deed the first time this application has been run, and
         ///  increases the appwide setting telling how many times the application has been run.
         /// </summary>
-        private void SetupRoutine()
+        private void TimesRunStartupRoutine()
         {
             if (Settings.Default.NumberOfTimesRun == 0)
             {
@@ -64,9 +95,7 @@ namespace Panda_20
         private void FirstTimeRun()
         {
             // Configuration of Message Box
-            String messageBoxText = "This program can launch automatically as soon as Windows starts." + "\n\n" +
-                                    "HINT: You can change the settings yourself in the Options menu of the application at a later time" + "\n\n" +
-                                    "Would you like to set the application to start automatically?";
+            String messageBoxText = "Would you like the application to start automatically when Windows starts?";
             String caption = "Panda: Start application automatically?";
             MessageBoxButton button = MessageBoxButton.YesNo;
             MessageBoxImage icon = MessageBoxImage.Question;
